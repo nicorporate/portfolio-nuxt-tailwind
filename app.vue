c4e591fda9a6cb09b6a78eabb416295b866b9ba8
Niko Aji Nugroho <nikonugroho@Nikos-MacBook-Air.local>
2025-07-05 23:09:17 +0700

    start using NuxtLayout

--- app.vue
+++ app.vue
@@ -1,14 +1,7 @@
 <template>
-  <div>
-    <!-- <NuxtRouteAnnouncer />
-    <NuxtWelcome /> -->
-    <nav>
-      <ul>
-        <li><NuxtLink to="/">Home</NuxtLink></li>
-        <li><NuxtLink to="/about">About</NuxtLink></li>
-        <li><NuxtLink to="/blog">Blog</NuxtLink></li>
-      </ul>
-    </nav>
-    <NuxtPage />
-  </div>
+    <!-- <NuxtPage /> -->
+
+    <NuxtLayout name="app">
+      <NuxtPage />
+    </NuxtLayout>
 </template>
