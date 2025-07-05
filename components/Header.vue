c48ccbb66b326bb18a839bc67c999ccb614959a7
Niko Aji Nugroho <nikonugroho@Nikos-MacBook-Air.local>
2025-07-06 01:26:24 +0700

    Separating page into components

---
+++ components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+      <header class="flex justify-between items-center">
+        <div>
+          <NuxtLink to="/" class="text-xl font-semibold link">Porto</NuxtLink>
+        </div>
+        <nav class="font-mono">
+          <ul class="flex space-x-4">
+            <li><NuxtLink to="/" class="link">Home</NuxtLink></li>
+            <li><NuxtLink to="/about" class="link">About</NuxtLink></li>
+            <li><NuxtLink to="/blog" class="link">Blog</NuxtLink></li>
+          </ul>
+        </nav>
+      </header>
+</template>
+
+<style scoped>
+.link{
+    @apply p-2 hover:bg-gray-200;
+}
+</style>
--- layouts/app.vue
+++ layouts/app.vue
@@ -1,17 +1,6 @@
 <template>
     <div class="container mx-auto">
-      <header class="flex justify-between items-center">
-        <div>
-          <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200">Porto</NuxtLink>
-        </div>
-        <nav class="font-mono">
-          <ul class="flex space-x-4">
-            <li><NuxtLink to="/">Home</NuxtLink></li>
-            <li><NuxtLink to="/about">About</NuxtLink></li>
-            <li><NuxtLink to="/blog">Blog</NuxtLink></li>
-          </ul>
-        </nav>
-      </header>
+      <Header />
       <main class="p-2">
         <slot />
       </main>
