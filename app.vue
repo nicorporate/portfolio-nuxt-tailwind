240c754f17d93b49b0b84698005592729916398e
Niko Aji Nugroho <nikonugroho@Nikos-MacBook-Air.local>
2025-07-05 15:17:06 +0700

    setup nuxt, adding pages dir, adding About & Blog page using NuxtPage, and routing each page using NuxtLink

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <!-- <NuxtRouteAnnouncer />
+    <NuxtWelcome /> -->
+    <nav>
+      <ul>
+        <li><NuxtLink to="/">Home</NuxtLink></li>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="/blog">Blog</NuxtLink></li>
+      </ul>
+    </nav>
+    <NuxtPage />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Nuxt Routing set up successfully</h1>
+        <p>Current route: {{ route.path }}</p>
+        <a href=""></a>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+const route = useRoute()
+
+</script>
