236134564b67d040588cf9a883a3397a3aca6479
Niko Aji Nugroho <nikonugroho@Nikos-MacBook-Air.local>
2025-07-06 21:04:24 +0700

    fetching using useFetch

--- pages/project.vue
+++ pages/project.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
         <h1>This is project page</h1>
+        <section v-if="pending">Loading...</section>
+        <section v-else-if="error">Something went wrong</section>
+        <section v-else>{{ data }}</section>
     </div>
 </template>
 
@@ -11,6 +14,6 @@ definePageMeta({
 useHead({
   title:'Project'
 })
-const project = await $fetch('https://api.github.com/users/nicorporate/repos');
-console.log(project)
+const {error, pending, data} = await useFetch('https://api.github.com/users/nicorporate/repos');
+// console.log(project)
 </script>
